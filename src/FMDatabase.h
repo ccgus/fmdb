40498307f9958511055f882df39b000f40cb3422
Rob Ryan <robert.ryan@mindspring.com>
2013-06-04 14:35:39 -0400

    Dramatically simplify introduction

--- src/FMDatabase.h
+++ src/FMDatabase.h
@@ -48,190 +48,24 @@
 
 /** A SQLite ([http://sqlite.org/](http://sqlite.org/)) Objective-C wrapper.
  
- ### The FMDB Mailing List:
- [http://groups.google.com/group/fmdb](http://groups.google.com/group/fmdb)
-
- ### Read the SQLite FAQ:
- [http://www.sqlite.org/faq.html](http://www.sqlite.org/faq.html)
-
- Since FMDB is built on top of SQLite, you're going to want to read this page top to bottom at least once.  And while you're there, make sure to bookmark the SQLite Documentation page: [http://www.sqlite.org/docs.html](http://www.sqlite.org/docs.html)
-
- ### The FMDB Source Code:
- The source code is available on GitHub at [https://github.com/ccgus/fmdb](https://github.com/ccgus/fmdb)
-
- ### Automatic Reference Counting (ARC) or Manual Memory Management?
- You can use either style in your Cocoa project.  FMDB Will figure out which you are using at compile time and do the right thing.
-
  ### Usage
- There are three main classes in FMDB:
+ The three main classes in FMDB are:
 
- 1. `FMDatabase` - Represents a single SQLite database.  Used for executing SQL statements.
- 2. `<FMResultSet>` - Represents the results of executing a query on an `FMDatabase`.
- 3. `<FMDatabaseQueue>` - If you're wanting to perform queries and updates on multiple threads, you'll want to use this class.  It's described in the "Thread Safety" section below.
+ - `FMDatabase` - Represents a single SQLite database.  Used for executing SQL statements.
+ - `<FMResultSet>` - Represents the results of executing a query on an `FMDatabase`.
+ - `<FMDatabaseQueue>` - If you want to perform queries and updates on multiple threads, you'll want to use this class.
 
- #### Database Creation
- An `FMDatabase` is created with a path to a SQLite database file.  This path can be one of these three:
-
- 1. A file system path.  The file does not have to exist on disk.  If it does not exist, it is created for you.
- 2. An empty string (`@""`).  An empty database is created at a temporary location.  This database is deleted with the `FMDatabase` connection is closed.
- 3. `nil`.  An in-memory database is created.  This database will be destroyed with the `FMDatabase` connection is closed.
-
- For example, to create/open a database in your Mac OS X `tmp` folder:
-
-    FMDatabase *db = [FMDatabase databaseWithPath:@"/tmp/tmp.db"];
-
- Or, in iOS, you might open a database in the app's `Documents` directory:
-
-    NSString *docsPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
-    NSString *dbPath   = [docsPath stringByAppendingPathComponent:@"test.db"];
-    FMDatabase *db     = [FMDatabase databaseWithPath:dbPath];
-
- (For more information on temporary and in-memory databases, read the sqlite documentation on the subject: [http://www.sqlite.org/inmemorydb.html](http://www.sqlite.org/inmemorydb.html))
-
- #### Opening
-
- Before you can interact with the database, it must be opened (see `<open>`).  Opening fails if there are insufficient resources or permissions to open and/or create the database.
-
-    if (![db open]) {
-        [db release];
-        return;
-    }
-
- #### Executing Updates
-
- Any sort of SQL statement which is not a `SELECT` statement qualifies as an update.  This includes `CREATE`, `UPDATE`, `INSERT`, `ALTER`, `COMMIT`, `BEGIN`, `DETACH`, `DELETE`, `DROP`, `END`, `EXPLAIN`, `VACUUM`, and `REPLACE` statements (plus many more).  Basically, if your SQL statement does not begin with `SELECT`, it is an update statement.
-
- Executing updates returns a single value, a `BOOL`.  A return value of `YES` means the update was successfully executed, and a return value of `NO` means that some error was encountered.  You may invoke the `<lastErrorMessage>` and `<lastErrorMessage>` methods to retrieve more information.
-
- #### Executing Queries
-
- A `SELECT` statement is a query and is executed via one of the `-executeQuery...` methods.
-
- Executing queries returns an `<FMResultSet>` object if successful, and `nil` upon failure.  Like executing updates, there is a variant that accepts an `NSError **` parameter.  Otherwise you should use the `<lastErrorMessage>` and `<lastErrorMessage>` methods to determine why a query failed.
-
- In order to iterate through the results of your query, you use a `while()` loop.  You also need to "step" (via `<[FMResultSet next]>`) from one record to the other.  With FMDB, the easiest way to do that is like this:
-
-    FMResultSet *s = [db executeQuery:@"SELECT * FROM myTable"];
-    while ([s next]) {
-        //retrieve values for each record
-    }
-
- You must always invoke `<[FMResultSet next]>` before attempting to access the values returned in a query, even if you're only expecting one:
-
-    FMResultSet *s = [db executeQuery:@"SELECT COUNT(*) FROM myTable"];
-    if ([s next]) {
-        int totalCount = [s intForColumnIndex:0];
-    }
-
- `<FMResultSet>` has many methods to retrieve data in an appropriate format:
-
- - `<[FMResultSet intForColumn:]>`
- - `<[FMResultSet longForColumn:]>`
- - `<[FMResultSet longLongIntForColumn:]>`
- - `<[FMResultSet boolForColumn:]>`
- - `<[FMResultSet doubleForColumn:]>`
- - `<[FMResultSet stringForColumn:]>`
- - `<[FMResultSet dateForColumn:]>`
- - `<[FMResultSet dataForColumn:]>`
- - `<[FMResultSet dataNoCopyForColumn:]>`
- - `<[FMResultSet UTF8StringForColumnName:]>`
- - `<[FMResultSet objectForColumnName:]>`
-
- Each of these methods also has a `{type}ForColumnIndex:` variant that is used to retrieve the data based on the position of the column in the results, as opposed to the column's name.
-
- Typically, there's no need to `<[FMResultSet close]>` an `<FMResultSet>` yourself, since that happens when either the result set is deallocated, or the parent database is closed.
-
- #### Closing
-
- When you have finished executing queries and updates on the database, you should `<close>` the `FMDatabase` connection so that SQLite will relinquish any resources it has acquired during the course of its operation.
-
-    [db close];
-
- #### Transactions
-
- `FMDatabase` can begin and commit a transaction by invoking one of the appropriate methods or executing a begin/end transaction statement.
-
- #### Data Sanitization
-
- When providing a SQL statement to FMDB, you should not attempt to "sanitize" any values before insertion.  Instead, you should use the standard SQLite binding syntax:
-
-    INSERT INTO myTable VALUES (?, ?, ?)
-
- The `?` character is recognized by SQLite as a placeholder for a value to be inserted.  The execution methods all accept a variable number of arguments (or a representation of those arguments, such as an `NSArray`, `NSDictionary`, or a `va_list`), which are properly escaped for you.
-
- Alternatively, you may use named parameters syntax:
-
-    INSERT INTO myTable VALUES (:id, :name, :value)
-
- The parameters *must* start with a colon. SQLite itself supports other characters, but internally the Dictionary keys are prefixed with a colon, do **not** include the colon in your dictionary keys.
-
-    NSDictionary *argsDict = [NSDictionary dictionaryWithObjectsAndKeys:@"My Name", @"name", nil];
-    [db executeUpdate:@"INSERT INTO myTable (name) VALUES (:name)" withParameterDictionary:argsDict];
-
- Thus, you SHOULD NOT do this (or anything like this):
-
-    [db executeUpdate:[NSString stringWithFormat:@"INSERT INTO myTable VALUES (%@)", @"this has \" lots of ' bizarre \" quotes '"]];
-
- Instead, you SHOULD do:
-
-    [db executeUpdate:@"INSERT INTO myTable VALUES (?)", @"this has \" lots of ' bizarre \" quotes '"];
-
- All arguments provided to the [`executeUpdate:`](<executeUpdate:>) method (or any of the variants that accept a `va_list` as a parameter) must be objects.  The following will not work (and will result in a crash):
-
-    [db executeUpdate:@"INSERT INTO myTable VALUES (?)", 42];
-
- The proper way to insert a number is to box it in an `NSNumber` object:
-
-    [db executeUpdate:@"INSERT INTO myTable VALUES (?)", [NSNumber numberWithInt:42]];
-
- Alternatively, you can use the [`executeUpdateWithFormat:`](<executeUpdateWithFormat:>) variant to use `NSString`-style substitution:
-
-    [db executeUpdateWithFormat:@"INSERT INTO myTable VALUES (%d)", 42];
-
- Internally, the [`executeUpdateWithFormat:`](<executeUpdateWithFormat:>) methods are properly boxing things for you.  The following percent modifiers are recognized:  `%@`, `%c`, `%s`, `%d`, `%D`, `%i`, `%u`, `%U`, `%hi`, `%hu`, `%qi`, `%qu`, `%f`, `%g`, `%ld`, `%lu`, `%lld`, and `%llu`.  Using a modifier other than those will have unpredictable results.  If, for some reason, you need the `%` character to appear in your SQL statement, you should use `%%`.
-
- ### Using FMDatabaseQueue and Thread Safety.
-
- Using a single instance of `FMDatabase` from multiple threads at once is a bad idea.  It has always been OK to make a `FMDatabase` object *per thread*.  Just don't share a single instance across threads, and definitely not across multiple threads at the same time.  Bad things will eventually happen and you'll eventually get something to crash, or maybe get an exception, or maybe meteorites will fall out of the sky and hit your Mac Pro.  *This would suck*.
-
- Instead, use <FMDatabaseQueue>.  It's your friend and it's here to help.
+ ### See also
  
- ### Making custom SQLite functions, based on blocks.
-
- You can do this!  For an example, see <makeFunctionNamed:maximumArguments:withBlock:>).
-
- ---
+ - `<FMDatabasePool>` - A pool of `FMDatabase` objects.
+ - `<FMStatement>` - A wrapper for `sqlite_stmt`.
  
- ### History
-
- The history and changes are availbe on its [GitHub page](https://github.com/ccgus/fmdb) and are summarized in the "CHANGES_AND_TODO_LIST.txt" file.
-
- ### Contributors
-
- The contributors to FMDB are contained in the "Contributors.txt" file.
-
- ### Reporting bugs
-
- Reduce your bug down to the smallest amount of code possible.  You want to make it super easy for the developers to see and reproduce your bug.  If it helps, pretend that the person who can fix your bug is active on shipping 3 major products, works on a handful of open source projects, has a newborn baby, and is generally very very busy.
-
- And we've even added a template function to `fmdb.m` (FMDBReportABugFunction) in the FMDB distribution to help you out:
-
- - Open up fmdb project in Xcode.
- - Open up main.m and modify the FMDBReportABugFunction to reproduce your bug.
- - Setup your table(s) in the code.
- - Make your query or update(s).
- - Add some assertions which demonstrate the bug.
-
- Then you can bring it up on the FMDB mailing list by showing your nice and compact FMDBReportABugFunction, or you can report the bug via the github FMDB bug reporter.
-
- **Optional:**
-
- Figure out where the bug is, fix it, and send a patch in or bring that up on the mailing list.  Make sure all the other tests run after your modifications.
-
- ### License
+ ### External links
  
- The license for FMDB is contained in the "License.txt" file.
-  
+ - [SQLite web site](http://sqlite.org/)
+ - [FMDB mailing list](http://groups.google.com/group/fmdb)
+ - [SQLite FAQ](http://www.sqlite.org/faq.html)
+ 
  @warning Do not instantiate a single `FMDatabase` object and use it across multiple threads. Instead, use `<FMDatabaseQueue>`.
 
  */
