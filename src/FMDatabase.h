568a145649697d65780803d110797fe76ff2acef
Graham Dennis <graham.dennis@gmail.com>
2013-11-23 09:21:04 +1100

    Add a comment explaining the use of OS_OBJECT_USE_OBJC

--- src/FMDatabase.h
+++ src/FMDatabase.h
@@ -24,11 +24,14 @@
 
     #define FMDBRelease(__v)
 
-	#if OS_OBJECT_USE_OBJC
-		#define FMDBDispatchQueueRelease(__v)
-	#else
-		#define FMDBDispatchQueueRelease(__v) (dispatch_release(__v));
-	#endif
+// If OS_OBJECT_USE_OBJC=1, then the dispatch objects will be treated like ObjC objects
+// and will participate in ARC.
+// See the section on "Dispatch Queues and Automatic Reference Counting" in "Grand Central Dispatch (GCD) Reference" for details. 
+    #if OS_OBJECT_USE_OBJC
+        #define FMDBDispatchQueueRelease(__v)
+    #else
+        #define FMDBDispatchQueueRelease(__v) (dispatch_release(__v));
+    #endif
 #endif
 
 #if !__has_feature(objc_instancetype)
