efdf7c56799d335c061e70bb8f7ecd876e7a416d
Jonathan Gander <j.gander@edsi-tech.com>
2015-06-30 08:00:21 +0200

    Syntax

--- src/fmdb/FMDatabase.m
+++ src/fmdb/FMDatabase.m
@@ -1315,8 +1315,9 @@ static NSString *FMDBEscapeSavePointName(NSString *savepointName) {
         return err;
     }
     
-	if (block)
-		block(&shouldRollback);
+    if (block) {
+        block(&shouldRollback);
+    }
     
     if (shouldRollback) {
         // We need to rollback and release this savepoint to remove it
@@ -1357,8 +1358,9 @@ void FMDBBlockSQLiteCallBackFunction(sqlite3_context *context, int argc, sqlite3
 #else
     void (^block)(sqlite3_context *context, int argc, sqlite3_value **argv) = (__bridge id)sqlite3_user_data(context);
 #endif
-	if (block)
-		block(context, argc, argv);
+    if (block) {
+        block(context, argc, argv);
+    }
 }
 
 
