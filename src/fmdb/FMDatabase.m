61b9fb76d6af2fd2e1ce899fb6673e3f81e43ebb
Robert M. Ryan <robert.ryan@mindspring.com>
2017-06-03 13:13:41 -0700

    Version 2.7.1

--- src/fmdb/FMDatabase.m
+++ src/fmdb/FMDatabase.m
@@ -98,7 +98,7 @@ NS_ASSUME_NONNULL_END
 }
 
 + (NSString*)FMDBUserVersion {
-    return @"2.7.0";
+    return @"2.7.1";
 }
 
 // returns 0x0240 for version 2.4.  This makes it super easy to do things like:
