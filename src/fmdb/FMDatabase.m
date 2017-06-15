1bc8ed2cf088937ff1e10646f9087c7fe58cf905
Andrew Liu <andrew@r65labs.com>
2017-06-15 12:59:50 -0400

    Update FMDatabase import if using SQLCipher

--- src/fmdb/FMDatabase.m
+++ src/fmdb/FMDatabase.m
@@ -4,8 +4,10 @@
 
 #if FMDB_SQLITE_STANDALONE
 #import <sqlite3/sqlite3.h>
-#else
+#elseifdef SQLITE_HAS_CODEC
 #import <SQLCipher/sqlite3.h>
+#else
+#import <sqlite3.h>
 #endif
 
 @interface FMDatabase () {
